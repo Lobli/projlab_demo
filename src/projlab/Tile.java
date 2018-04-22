e22c7dea54b315f25a9d9160fad0a032fa6bd5ce
Bertalan Körmendy <korber314@gmail.com>
2018-04-22 23:10:11 +0200

    Kommentek

--- src/projlab/Tile.java
+++ src/projlab/Tile.java
@@ -6,13 +6,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Tile {
+    /**
+        A mező szomszédai az elérési irány szerint indexelve.
+    */
     private HashMap<Direction, Tile> neighbors = new HashMap<>();
+    
+    /**
+        Megadja, hogy melyik GameObject (ha egyáltalán akármelyik) foglalja el
+        ezt a mezőt.
+        null-ra (üres) inicializálva.
+    */
     protected GameObject occupiedBy = null;
+    
+    /**
+        Megadja, hogy milyen anyag (ha egyáltalán akármelyik) borítja
+        ezt a mezőt.
+        null-ra (tiszta) inicializálva.
+    */
     private Material coveredBy = null;
 
-    public Tile() {
-    }
-
+    /**
+        Egy adott Tile-t olvas be a térképfájlból.
+        
+        @param m            Ehhez a térképhez tartozik a beolvasott tile
+        @param tokens       a térképfile tokenekre szétszedve
+        @returns            a tile megfelelően inicializált példányával.
+        @throws SyntaxError ha nem a térképleíró nyelv szabályainak megfelelő struktúrát
+                            talál
+    */
     static Tile fromString(ArrayList<String> tokens, Map m) throws SyntaxError {
         Tile t = new Tile();
 
@@ -40,14 +61,38 @@ public class Tile {
         return t;
     }
 
-    public void leave(Worker go) {
+    /**
+        Definiálja az alapértelemzett viselkedést, ami akkor történik, amikor egy
+        Worker elhagyja ezt a mezőt.
+        
+        @param worker a mezőt elhagyó worker
+        @see Worker
+    */
+    public void leave(Worker worker) {
+        this.occupiedBy = null;
+    }
+    
+    /**
+        Definiálja az alapértelemzett viselkedést, ami akkor történik, amikor egy
+        Box elhagyja ezt a mezőt.
+        
+        @param box a mezőt elhagyó doboz
+        
+        @see Box
+    */
+    public void leave(Box box) {
         this.occupiedBy = null;
     }
 
-    public void leave(Box go) {
-        this.occupiedBy = null;
-    }
-
+    /**
+        Definiálja az alapértelemzett viselkedést, ami akkor történik, amikor egy
+        Worker belép erre a mezőre.
+        
+        @param worker       a belépő munkás
+        @param direction    a mozgás iránya
+        
+        @see Worker
+    */
     public void enter(Worker worker, Direction direction) {
         if (occupiedBy != null) {
             occupiedBy.push(direction);
@@ -56,13 +101,31 @@ public class Tile {
         setOccupiedBy(worker);
     }
 
+    /**
+        Definiálja az alapértelemzett viselkedést, ami akkor történik, amikor egy
+        Box belép erre a mezőre.
+        
+        @param box          a belépő doboz
+        @param direction    a mozgás iránya
+        
+        @see Box
+    */
     public void enter(Box box, Direction direction) {
         if (occupiedBy != null) {
             occupiedBy.push(direction);
         }
         setOccupiedBy(box);
     }
-
+    
+    /**
+        Megadja, hogy egy adott munkás be tud-e lépni erre a mezőre.
+        
+        @param worker   A belépő munkás
+        @param goingIn  A munkás mozgásának iránya
+        @param force    A munkás által kifejtett nyomóerő
+        
+        @return         igaz, ha beléphet, hamis, ha nem léphet be
+    */
     public boolean canBeEnteredBy(Worker worker, Direction goingIn, double force) {
         if (occupiedBy != null) {
             double newForce = coveredBy == null ? force : force * coveredBy.getCoefficient();
@@ -73,6 +136,15 @@ public class Tile {
         return true;
     }
 
+    /**
+        Megadja, hogy egy adott doboz be tud-e lépni erre a mezőre.
+        
+        @param box      A belépő doboz
+        @param goingIn  A munkás mozgásának iránya
+        @param force    A doboz által kifejtett nyomóerő
+        
+        @return         igaz, ha beléphet, hamis, ha nem léphet be
+    */
     public boolean canBeEnteredBy(Box box, Direction goingIn, double force){
         double newForce = coveredBy == null ? force : force * coveredBy.getCoefficient();
         if (occupiedBy != null)
@@ -80,18 +152,44 @@ public class Tile {
         return true;
     }
 
+    /**
+        Beállítja a coveredBy értékét
+        @param coveredBy    az új anyag, ami a mezőt borítja
+    */
     public void setCoveredBy(Material coveredBy) {
         this.coveredBy = coveredBy;
     }
+
+    /**
+        Beállítja a szomszédot egy adott irányban
+        
+        @param direction    a kívánt irány
+        @param tile         a megadott irányban lévő szomszéd
+    */
 
     public void setNeighborInDirection(Direction direction, Tile tile) {
         this.neighbors.put(direction, tile);
     }
 
+    /**
+        Megadja, hogy egy adott irányban melyik mező szomszédos ezzel a mezővel.
+        
+        @param direction    a kívánt irány
+        
+        @returns            null ha nincs szomszáda az adott irányban, Tile instance
+                            ha létezik
+    */
     public Tile getNeighborInDirection(Direction direction){
         return neighbors.get(direction);
     }
-
+    
+    /**
+        Beállítja az occupiedBy értékét
+        
+        @param newComer Az új GameObject ami a mezőn van
+        
+        @see GameObject
+    */
     public void setOccupiedBy(GameObject newComer) {
         if (occupiedBy != null) {
             occupiedBy.setTile(null);
@@ -104,6 +202,12 @@ public class Tile {
         }
     }
 
+    /**
+        Visszatér a mező jelkódjával, ha üres, vagy a rajta lévő anyag vagy GameObject
+        jelkódjával
+        
+        @returns    A jelkód
+    */
     public String toString() {
         if (occupiedBy != null)
             return occupiedBy.toString();
