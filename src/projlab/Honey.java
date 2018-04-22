e3d47f1c125f66031f34f33f49da7db4f49672ea
EszterBajmoczy <36538209+EszterBajmoczy@users.noreply.github.com>
2018-04-22 23:45:41 +0200

    kommentek

--- src/projlab/Honey.java
+++ src/projlab/Honey.java
@@ -1,10 +1,18 @@
 package projlab;
 
+/**
+* A méz együtthatójával működő osztály.
+*/
 public class Honey extends Material {
+    //Az osztály konstruktora
     public Honey(){
         coefficient = 1.5;
     }
 
+    /**
+    * Visszatér az anyag jelkódjával.
+    * @return A jelkód
+    */
     @Override
     public String toString() {
         return "h ";
