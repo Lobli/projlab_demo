1bcab96ba5696612ef83582d518b91b49bef8786
Bertalan Körmendy <korber314@gmail.com>
2018-04-22 23:39:19 +0200

    javadoc

--- src/projlab/Oil.java
+++ src/projlab/Oil.java
@@ -1,10 +1,18 @@
 package projlab;
 
 public class Oil extends Material {
+    /**
+        Konstruktor, beállítja az anyag együtthatóját
+    */
     public Oil(){
         coefficient = 0.5;
     }
-
+    
+    /**
+        Visszatér az anyag jelkódjával.
+        
+        @return     A jelkód
+    */
     @Override
     public String toString() {
         return "o ";
