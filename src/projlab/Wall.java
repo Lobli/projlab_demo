7302d0e8fa75eedacf3af571363aa47e0efce4cf
Bertalan Körmendy <korber314@gmail.com>
2018-04-22 22:36:34 +0200

    Kommentek

--- src/projlab/Wall.java
+++ src/projlab/Wall.java
@@ -1,26 +1,33 @@
 package projlab;
 
 public class Wall extends Tile {
-
-    static Wall fromString(String[] args){
-        return new Wall();
-    }
-
+    
+    /**
+        A belépő munkást eltávolítja a játékból.
+        Ez szimulálja a munkás összepréselését.
+    */
     @Override
     public void enter(Worker worker, Direction direction) {
         worker.removeFromGame();
     }
-
+    
+    /**
+    */
     @Override
     public boolean canBeEnteredBy(Box box, Direction goingIn, double force) {
         return false;
     }
 
+    /**
+    */
     @Override
     public boolean canBeEnteredBy(Worker worker, Direction goingIn, double force) {
         return false;
     }
 
+    /**
+        Visszatér a fal jelkódjával ("W").
+    */
     @Override
     public String toString() {
         return "W ";
