c28ebc1bdf590d4c3c1fdd44ef540eb04294c13a
Bertalan Körmendy <korber314@gmail.com>
2018-04-22 23:41:37 +0200

    javadoc

--- src/projlab/NotImplementedException.java
+++ src/projlab/NotImplementedException.java
@@ -1,10 +1,21 @@
 package projlab;
 
+/**
+    Exception, ami akkor dobódik, ha a program egy adott objektumban szereplő, de nem értlmezett
+    függvényt akar hívni. Ez az öröklés miatt fodulhat elő (pl Box::move).
+*/
 public class NotImplementedException extends Exception {
+    /**
+        Konstruktor
+    */
     public NotImplementedException() {
         super();
     }
-
+    
+    /**
+        Konstruktor
+        @param message  a hibaüzenet
+    */
     public NotImplementedException(String message) {
         super(message);
     }
