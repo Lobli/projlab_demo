5d1c5f051fcba46c4e15af68cda1504793d5247c
Bertalan Körmendy <korber314@gmail.com>
2018-04-23 00:03:02 +0200

    javadoc

--- src/projlab/Map.java
+++ src/projlab/Map.java
@@ -9,17 +9,54 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Map {
+    /**
+        A térkép neve
+    */
     String name;
+    
+    /**
+        A térkép sorainak száma
+    */
     private int height;
+    
+    /**
+        A térkép oszlopainak száma
+    */
     private int width;
 
+    /**
+        A térképen lévő mezők listája
+    */
     private ArrayList<Tile> tiles  = new ArrayList<>();
+    
+    /**
+        A térképen lévő munkások listája
+    */
     ArrayList<Worker> workers= new ArrayList<>();
+    
+    /**
+        A térképen lévő dobozok listája
+    */
     ArrayList<Box> boxes = new ArrayList<>();
 
+    /**
+        A térképen lévő, még meg nem oldott switch-hole összerendeléskhez
+        tartozó tároló
+    */
     private HashMap<String, Tile> switches = new HashMap<>();
+    
+    /**
+        A térképen lévő, még meg nem oldott worker-targettile összerendeléskhez
+        tartozó tároló
+    */
     private HashMap<String, TargetTile> targetTiles = new HashMap<>();
 
+    /**
+        Konstruktor
+        @param path         a térképfile elérési útvonala
+        @throws SyntaxError ha a térképfile nem a szintakitkai szabályoknak megfelelően van leírva
+        @throws IOException ha a térképfile-t nem találja a program vagy a fájl memóriába olvasása közben hiba történt
+    */
     public Map(String path) throws SyntaxError, IOException {
         String map = new String(Files.readAllBytes(Paths.get(path)));
         read_from(tokenize(map));
@@ -30,7 +67,11 @@ public class Map {
             configureNeighborsMultiLine();
 
     }
-
+    
+    /**
+        @param tokens   a térképfájl tokenekre felbontva
+        @throws SyntaxError ha a feldolgozott szegmens nem a szintakitkai szabályoknak megfelelően van leírva
+    */
     private void read_from(ArrayList<String> tokens) throws SyntaxError {
         String token = tokens.remove(0);
         if (token.equals("[")) {
@@ -40,7 +81,12 @@ public class Map {
             parse_object(tokens);
         }
     }
-
+    
+    /**
+        Beolvas egy térkép objektumot szöveges reprezentációjából.
+        @param tokens   a térképfájl tokenekre felbontva
+        @throws SyntaxError ha a feldolgozott szegmens nem a szintakitkai szabályoknak megfelelően van leírva
+    */
     private void parse_map(ArrayList<String> tokens) throws SyntaxError {
         while( ! tokens.get(0).equals("}")){
             String next = tokens.remove(0);
@@ -62,7 +108,12 @@ public class Map {
         }
         tokens.remove(0);
     }
-
+    
+    /**
+        Beolvas egy sort egy Row objektum szöveges reprezentációjából.
+        @param tokens   a térképfájl tokenekre felbontva
+        @throws SyntaxError ha a feldolgozott szegmens nem a szintakitkai szabályoknak megfelelően van leírva
+    */
     private void parse_row(ArrayList<String> tokens) throws SyntaxError {
         while (! tokens.get(0).equals("}")) {
             if (tokens.get(0).equals(":cells")) {
@@ -75,6 +126,12 @@ public class Map {
         tokens.remove(0);
     }
 
+    /**
+        Beolvas egy Hole-t a szöveges reprezentációból.
+        @param tokens   a térképfájl tokenekre felbontva
+        @throws SyntaxError ha a feldolgozott szegmens nem a szintakitkai szabályoknak megfelelően van leírva
+        @see Hole
+    */
     private void parse_hole(ArrayList<String> tokens) throws SyntaxError {
         Hole h = new Hole();
         while (!tokens.get(0).equals("}")) {
@@ -107,7 +164,13 @@ public class Map {
         }
         tiles.add(h);
     }
-
+    
+    /**
+        Beolvas egy Switch-et a szöveges reprezentációból.
+        @param tokens   a térképfájl tokenekre felbontva
+        @throws SyntaxError ha a feldolgozott szegmens nem a szintakitkai szabályoknak megfelelően van leírva
+        @see Switch
+    */
     private void parse_switch(ArrayList<String> tokens) throws SyntaxError {
         Switch s = new Switch();
         if (tokens.remove(0).equals(":controlling")) {
@@ -126,6 +189,14 @@ public class Map {
         tiles.add(s);
     }
 
+    /**
+        Beolvas egy munkást a szöveges reprezentációból.
+        @param tokens   a térképfájl tokenekre felbontva
+        @param t        a mező, amin a feldogozandó Worker áll
+        @return         egy worker példány
+        @throws SyntaxError ha a feldolgozott szegmens nem a szintakitkai szabályoknak megfelelően van leírva
+        @see Worker
+    */
     public Worker parse_worker(ArrayList<String> tokens, Tile t) throws SyntaxError {
         Worker w = Worker.fromString(tokens, t);
         workers.add(w);
@@ -136,6 +207,12 @@ public class Map {
         return w;
     }
 
+    /**
+        Beolvas egy targettile-t a szöveges reprezentációból.
+        
+        @param tokens   a térképfájl tokenekre felbontva
+        @throws SyntaxError ha a feldolgozott szegmens nem a szintakitkai szabályoknak megfelelően van leírva
+    */
     private void parse_targettile(ArrayList<String> tokens) throws SyntaxError {
         if (tokens.remove(0).equals(":belongsTo")){
             TargetTile tt = new TargetTile();
@@ -154,12 +231,27 @@ public class Map {
             throw new SyntaxError();
     }
 
+    /**
+        Beolvas egy dobozt a szöveges reprezentációból.
+        
+        @param tokens   a térképfájl tokenekre felbontva
+        @param t        a mező, amin a feldogozandó Box áll
+        @return         egy Box példány
+        @throws SyntaxError ha a feldolgozott szegmens nem a szintakitkai szabályoknak megfelelően van leírva.
+        @see Box
+    */
     public Box parse_box(ArrayList<String> tokens, Tile t) throws SyntaxError {
         Box b = Box.fromString(tokens, t);
         boxes.add(b);
         return b;
     }
 
+    /**
+        Beolvas egy térképfile-objektumot.
+        
+        @param tokens   a térképfájl tokenekre felbontva
+        @throws SyntaxError ha a feldolgozott szegmens nem a szintakitkai szabályoknak megfelelően van leírva
+    */
     private void parse_object(ArrayList<String> tokens) throws SyntaxError {
         String type = tokens.remove(0);
         if (type.equals("Map")){
@@ -187,6 +279,13 @@ public class Map {
             throw new SyntaxError();
     }
 
+    /**
+        Beolvas egy térképfájl-szöveget.
+        
+        @param tokens   a térképfájl tokenekre felbontva
+        
+        @return a szöveg
+    */
     public static String parse_string(ArrayList<String> tokens){
         ArrayList<String> words = new ArrayList<>();
         while (! tokens.get(0).equals("'")) {
@@ -196,6 +295,14 @@ public class Map {
         return String.join(" ", words);
     }
 
+    /**
+        Beolvas egy térképfájl-számot.
+        
+        @param tokens   a térképfájl tokenekre felbontva
+        @throws SyntaxError ha a feldolgozott szegmens nem a szintakitkai szabályoknak megfelelően van leírva
+        
+        @return a beolvasott szám
+    */
     public static int parse_number(ArrayList<String> tokens) throws SyntaxError {
         String str = tokens.remove(0);
         for (int i = 0; i < str.length(); i++)
@@ -204,6 +311,12 @@ public class Map {
         return Integer.parseInt(str);
     }
 
+    /**
+        Beolvas egy térképfájl-listát.
+        
+        @param tokens   a térképfájl tokenekre felbontva
+        @throws SyntaxError ha a feldolgozott szegmens nem a szintakitkai szabályoknak megfelelően van leírva
+    */
     private void parse_list(ArrayList<String> tokens) throws SyntaxError {
         while (!tokens.get(0).equals("]")) {
             read_from(tokens);
@@ -211,6 +324,12 @@ public class Map {
         tokens.remove(0);
     }
 
+    /**
+        Tokenekre bont egy térképfájlt
+        
+        @param map  a térképfájl tartalma String-ben
+        @return     a tokenek listáját
+    */
     private ArrayList<String> tokenize(String map){
         String sig_chars[] = { "'", "[", "]", "(", ")", "{", "}" };
 
@@ -230,7 +349,15 @@ public class Map {
 
         return ret;
     }
-
+    
+    /**
+        A workerek listájából megkereses egy példányt a neve alapján
+        
+        @param name a Worker neve
+        @return     null ha nem találta a keresett Workert, Worker példány ha igen        
+        
+        @see Worker
+    */
     private Worker getWorker(String name){
         for (Worker w : workers)
             if (w.getName().equals(name))
@@ -238,6 +365,9 @@ public class Map {
         return null;
     }
 
+    /**
+        Kinyomtatja a konzolra a térképet karaketeres reprezentációban
+    */
     public void printMap(){
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
@@ -247,6 +377,9 @@ public class Map {
         }
     }
 
+    /**
+        Egy többsoros térképen bekonfigurálja a mezők szomszédait
+    */
     private void configureNeighborsMultiLine(){
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
@@ -305,6 +438,9 @@ public class Map {
         }
     }
 
+    /**
+        Egy egysoros térképen bekonfigurálja a mezők szomszédait
+    */
     private void configureNeighborsSingleLine(){
         if (width == 1 && height == 1)
             return;
@@ -323,5 +459,4 @@ public class Map {
             System.out.println();
         }
     }
-
 }
