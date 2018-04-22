19b380839e46b41c576b796befd2189bcb525e7a
Bertalan Körmendy <korber314@gmail.com>
2018-04-22 23:22:56 +0200

    javadoc

--- src/projlab/SyntaxError.java
+++ src/projlab/SyntaxError.java
@@ -1,10 +1,21 @@
 package projlab;
 
+/**
+    Saját SyntaxError osztály, ami a térképleírónyelven megírt térkép beolvasásakor az
+    esetleges hibák jelzésére használatos
+*/
 public class SyntaxError extends Exception {
+    /**
+        Konstruktor
+    */
     public SyntaxError() {
         super();
     }
 
+    /**
+        Konstruktor
+        @param message  a hibaüzenet
+    */
     public SyntaxError(String message) {
         super(message);
     }
