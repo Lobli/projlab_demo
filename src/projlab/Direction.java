1b68dfae2ef2748b237a0686e963fe48a66fc9b5
EszterBajmoczy <36538209+EszterBajmoczy@users.noreply.github.com>
2018-04-22 22:59:46 +0200

    kommentek

--- src/projlab/Direction.java
+++ src/projlab/Direction.java
@@ -1,17 +1,35 @@
 package projlab;
 
+/**
+* Az irányokat meghatározó enum. A enum tagokon kívüli változók és függvények azért vannak, 
+* hogy a különböző irányoknak String reprezentációja is legyen.
+*/
 public enum Direction {
+    //Felfele irány ( a képernyőt nézve).
     UP("UP"),
+    //Jobbra irány
     RIGHT("RIGHT"),
+    //Lefele irány
     DOWN("DOWN"),
+    //Balra irány
     LEFT("LEFT");
 
+    /**
+    * Az irányokat meghatározó enum. A enum tagokon kívüli változók és függvények azért vannak, 
+    */
     private final String textRepresentation;
 
+    /**
+    * Az osztály konstruktora
+    */
     Direction(String textRepresentation){
         this.textRepresentation = textRepresentation;
     }
-
+    
+    /**
+    * Visszatér az adott enum tag szöveges reprezentációjával.
+    * @return az enum szöveges reprezentációja
+    */
     @Override
     public String toString() {
         return textRepresentation;
