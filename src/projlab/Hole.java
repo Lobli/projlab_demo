c086b77455e536c9e4bfa37d410060dc29ce1233
EszterBajmoczy <36538209+EszterBajmoczy@users.noreply.github.com>
2018-04-22 23:42:35 +0200

    kommentek

--- src/projlab/Hole.java
+++ src/projlab/Hole.java
@@ -1,19 +1,36 @@
 package projlab;
 
+/**
+*A lyuk viselkedését megvalósító Tile alosztály. 
+*/
 public class Hole extends Tile {
-
+    //Megadja, hogy nyitva vagy csukva van-e a lyuk.
     private boolean closed = true;
 
+    /**
+    * Az osztály konstruktora
+    */
     public Hole() { }
 
+    /**
+    * A closed setter-e.
+    */
     public void setClosed(boolean closed) {
         this.closed = closed;
     }
 
+    /**
+    * A closed getter-e.
+    * @return Igazat vagy hamisat ad vissza annak megfelelően, hogy nyitva van e a lyuk
+    */
     public boolean isClosed() {
         return closed;
     }
 
+    /**
+    * Az alap Tile enter-ét kiegészítő függvény, ami a Hole állapotának függvényében (nyitott v. csukott) eldönti, 
+    * hogy “megölilje”-e  a rálépő dobozt.
+    */
     @Override
     public void enter(Box b, Direction d) {
         if (closed)
@@ -22,7 +39,12 @@ public class Hole extends Tile {
             b.removeFromGame();
     }
 
-
+    /**
+    * Az alap Tile enter-ét kiegészítő függvény, ami a Hole állapotának függvényében (nyitott v. csukott) eldönti, 
+    * hogy “megölilje”-e  a rálépő munkást.
+    * @param w  A belépni akaró munkás
+    * @param d  Az irány ahonnan be akar lépni
+    */
     @Override
     public void enter(Worker w, Direction d) {
         if(closed)
@@ -32,6 +54,10 @@ public class Hole extends Tile {
 
     }
 
+    /**
+    * A Hole jelkódjával tér vissza, ami a függ az állapottól (nyitott v. csukott).
+    * @return A Hole jelkódja
+    */
     @Override
     public String toString() {
         if (closed){
