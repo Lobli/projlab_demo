422ada0d8c74b2da30ffe05502325bbbf5a9fed0
Bertalan Körmendy <korber314@gmail.com>
2018-04-22 23:45:26 +0200

    javadoc

--- src/projlab/Material.java
+++ src/projlab/Material.java
@@ -3,12 +3,23 @@ package projlab;
 import projlab.SyntaxError;
 
 public abstract class Material {
+    /**
+        Az anyag tapadási súrlódási együtthatója
+    */
     double coefficient;
 
+    /**
+        @return Az anyag tapadási súrlódási együtthatója
+    */
     public double getCoefficient() {
         return coefficient;
     }
 
+    /**
+        Beolvassa az anyagot a térképfájlból
+        
+        @throws SyntaxError ha nem értekmezett inputot kap
+    */
     static Material fromString(String type) throws SyntaxError {
         if (type.equals("Oil"))
             return new Oil();
