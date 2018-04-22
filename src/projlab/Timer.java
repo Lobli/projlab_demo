5f6cb4c9115e7596198d7e27da5a676ad47fa296
Bertalan Körmendy <korber314@gmail.com>
2018-04-22 22:44:01 +0200

    Kommentek

--- src/projlab/Timer.java
+++ src/projlab/Timer.java
@@ -3,15 +3,40 @@ package projlab;
 import java.util.Scanner;
 
 public class Timer {
+    /**
+        A jelenleg a játékból hátralévő időlépések száma.
+    */
     private int time;
+    
+    /**
+        Megadja, hogy a háttérben számoljon vissza a Timer vagy diszkrét időlépésenként
+        menjen.
+        
+        Háttérben futóra inicializálva.
+    */
     private boolean steppable = false;
+    
+    /**
+        Megadja, hogy éppen szünetre van-e állítva az óra.
+        Hamisra inicializálva.
+    */
     private boolean paused = false;
 
+    /**
+        Konstruktor
+       
+        @param time hány időlépésből álljon a játék
+        @param steppable léptethető legyen-e a timer
+    */
     public Timer(int time, boolean steppable) {
         this.steppable = steppable;
         this.time = time;
     }
-
+    
+    /**
+        Egy időlépést vezényel le. Figyelembe veszi, hogy van-e még hátra a játékból,
+        vagy szünetelve van-e a játék.
+    */
     public boolean tick() {
         Scanner s = new Scanner(System.in);
         if (paused) {
@@ -37,15 +62,24 @@ public class Timer {
         }
         return true;
     }
-
+    
+    /**
+        Megadja, hogy szünetelve van-e a játék.
+    */
     public boolean isPaused() {
         return paused;
     }
-
+    
+    /**
+        Leállítja a játékot azzal, hogy 0-ra állítja a hátralévő időt.
+    */
     public void stop(){
         time = 0;
     }
-
+    
+    /**
+        Megváltoztatja a paused állapotát (true -> false vagy false -> true)
+    */
     public void togglePaused() {
         paused = !paused;
     }
